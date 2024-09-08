e0b4f06591941676c4214986426a6b5a17cae6c4
Shivam Anand <shivam850anand@gmail.com>
2024-09-09 01:30:34 +0530

    done

--- src/components/layout/project/ProjectContainer.jsx
+++ src/components/layout/project/ProjectContainer.jsx
@@ -7,14 +7,24 @@ import { Typography } from "@material-tailwind/react";
 const ProjectContainer = () => {
   return (
     <>
-      <Typography variant="h3" className="w-[90%] mx-auto underline underline-offset-4 text-purple-400 font-Varela  ">HARD SURFACES</Typography>
+      <Typography
+        variant="h3"
+        className="w-[90%] mx-auto underline underline-offset-4 text-purple-400 font-Varela  "
+      >
+        HARD SURFACES
+      </Typography>
       <div className="grid md:grid-cols-3 py-10 gap-8   min-h-[89vh] place-items-center">
         {ThreeDmodelsData.map((model) => (
           <ModelsSection model={model} />
         ))}
       </div>
       <div className="w-[90%] mx-auto  pb-10">
-      <Typography variant="h3" className="w-[90%] mx-auto underline my-6 underline-offset-4 text-purple-400 font-Varela  ">HARD SURFACES</Typography>
+        <Typography
+          variant="h3"
+          className="w-[90%] mx-auto underline my-6 underline-offset-4 text-purple-400 font-Varela  "
+        >
+          OTHER-WORKS
+        </Typography>
         <ImageSlideShow />
       </div>
     </>
